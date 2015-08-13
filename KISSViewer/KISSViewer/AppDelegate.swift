b11f4eb9b07426ef10b4039283bd4100a631ccc2
Seth <Seth@Kyoko.local>
2015-08-13 13:46:57 -0300

    Initial commit

---
+++ KISSViewer/KISSViewer/AppDelegate.swift
@@ -0,0 +1,281 @@
+//
+//  AppDelegate.swift
+//  Borderless Image Viewer
+//
+//  Created by Seth Buxton on 2015-08-13.
+//  Copyright (c) 2015 ZAE Games. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    // The window that is shown when the user wants to open a file
+    @IBOutlet weak var openFileWindow: NSWindow!
+    
+    // The text field for the image path
+    @IBOutlet weak var filePathField: NSTextField!
+    
+    // The color well for the image background color
+    @IBOutlet weak var windowBackgroundColorWell: NSColorWell!
+    
+    // The checkbox for if the window should show the traffic lights
+    @IBOutlet weak var windowControlsCheckbox: NSButton!
+    
+    // The checkbox for if the window should float above the others
+    @IBOutlet weak var windowFloatsCheckbox: NSButton!
+    
+    // The checkbox for if the window should move to the active space
+    @IBOutlet weak var windowJoinCheckbox: NSButton!
+    
+    // The checkbox for if the window should have a shadow
+    @IBOutlet weak var windowShadowCheckbox: NSButton!
+    
+    // The checkbox for if the window should let users click through it
+    @IBOutlet weak var windowClickThroughCheckbox: NSButton!
+    
+    // When the user clicks the open menu item or pressed CMD+O
+    @IBAction func openMenuItemTriggered(sender: AnyObject) {
+        // Prompt to open a file
+        promptToOpen();
+    }
+    
+    // When the open button in the openFileWinodw is pressed...
+    @IBAction func openButtonPressed(sender: AnyObject) {
+        // Hide the open file window
+        openFileWindow.orderOut(self);
+        
+        // Initialize variables for all the window options
+        
+        // Shows traffic lights
+        var windowControlsState : Bool = false;
+        
+        // Float
+        var windowFloatsState : Bool = false;
+        
+        // Moveto active space
+        var windowJoinState : Bool = false;
+        
+        // Shadow
+        var windowShadowState : Bool = false;
+        
+        // Can click through
+        var windowClickThroughState : Bool = false;
+        
+        // Set all the bools to there respective values, based on the checkbox state
+        
+        if(windowControlsCheckbox.state == 1) {
+            // Its on, set it to true
+            windowControlsState = true;
+        }
+        else {
+            // Its off, set it to false
+            windowControlsState = false;
+        }
+        
+        if(windowFloatsCheckbox.state == 1) {
+            // Its on, set it to true
+            windowFloatsState = true;
+        }
+        else {
+            // Its off, set it to false
+            windowFloatsState = false;
+        }
+        
+        if(windowJoinCheckbox.state == 1) {
+            // Its on, set it to true
+            windowJoinState = true;
+        }
+        else {
+            // Its off, set it to false
+            windowJoinState = false;
+        }
+        
+        if(windowShadowCheckbox.state == 1) {
+            // Its on, set it to true
+            windowShadowState = true;
+        }
+        else {
+            // Its off, set it to false
+            windowShadowState = false;
+        }
+        
+        if(windowClickThroughCheckbox.state == 1) {
+            // Its on, set it to true
+            windowClickThroughState = true;
+        }
+        else {
+            // Its off, set it to false
+            windowClickThroughState = false;
+        }
+        
+        // Create new image viewer window with or variables
+        createNewViewer(NSURL(fileURLWithPath: filePathField.stringValue)!, windowControls: windowControlsState, floats: windowFloatsState, joins: windowJoinState, shadow: windowShadowState, clickThrough: windowClickThroughState, bgColor: windowBackgroundColorWell.color);
+    }
+    
+    // An array of all the opened windows
+    var windowArray : [NSWindow] = [NSWindow](count: 0, repeatedValue: NSWindow());
+    
+    // An array of all the opened image views
+    var imageViewArray : [NSImageView] = [NSImageView](count: 0, repeatedValue: NSImageView());
+    
+    // The current window we are opening
+    var currentWindow : Int = 0;
+    
+    // A temp variable to temporaroly store our new image view
+    var newImage : NSImage = NSImage();
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Hide the file open window
+        openFileWindow.orderOut(self);
+        
+        openFileWindow.titlebarAppearsTransparent = true;
+        
+        // Allow the user to input a color with an alpha value
+        NSColorPanel.sharedColorPanel().showsAlpha = true;
+    }
+    
+    // Prompts the user to open an image
+    func promptToOpen() {
+        // Center the open file window
+        openFileWindow.center();
+        
+        // Make the open file window key and in front
+        openFileWindow.makeKeyAndOrderFront(self);
+    }
+    
+    // Creates a new image viewer window
+    // url (NSURL): The URL to the image to open
+    // windowControls (Bool): Should the window show the traffic lights?
+    // floats (Bool): Should the iwndow float above the others?
+    // joins (Bool): Should the window move to the active space?
+    // shadow (Bool): Shoudl the window have a shadow?
+    // clickThrough (Bool): Can the user click through the window?
+    // bgColor (NSColor): The color to put for parts of the image that are transparent
+    func createNewViewer(url : NSURL, windowControls : Bool, floats : Bool, joins : Bool, shadow : Bool, clickThrough : Bool, bgColor : NSColor) {
+        // Create the new NSImage to hold our image we are loading
+        newImage = NSImage(contentsOfURL: url)!;
+        
+        // Add a new NSImageView to the array, to show the NSImage
+        imageViewArray.append(NSImageView(frame: NSRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height)));
+        
+        // Set the new image view's image to our new image we are loading
+        imageViewArray[currentWindow].image = newImage;
+        
+        // Set the image view resizing mask so it matches the width and height of the window
+        imageViewArray[currentWindow].autoresizingMask = NSAutoresizingMaskOptions.ViewHeightSizable | NSAutoresizingMaskOptions.ViewWidthSizable;
+        
+        // Set the NSImageView scaling to Axes Independent
+        imageViewArray[currentWindow].imageScaling = NSImageScaling.ImageScaleAxesIndependently;
+        
+        // If we want to show the traffic lights...
+        if(windowControls) {
+            // Create a new window with traffic lights
+            windowArray.append(NSWindow(
+                contentRect: NSScreen.mainScreen()!.frame,
+                styleMask: NSTitledWindowMask | NSFullSizeContentViewWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask,
+                backing: NSBackingStoreType.Buffered,
+                defer: false)
+            );
+        }
+        // If we dont want to show traffic lights...
+        if(!windowControls) {
+            // Create a new window
+            windowArray.append(NSWindow(
+                contentRect: NSScreen.mainScreen()!.frame,
+                styleMask: NSTitledWindowMask | NSFullSizeContentViewWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask,
+                backing: NSBackingStoreType.Buffered,
+                defer: false)
+            );
+            
+            // Hide the close button
+            var closeButton : NSButton = windowArray[currentWindow].standardWindowButton(NSWindowButton.CloseButton)!;
+            closeButton.hidden = true;
+            
+            // Hide the minimize button
+            var minimizeButton : NSButton = windowArray[currentWindow].standardWindowButton(NSWindowButton.MiniaturizeButton)!;
+            minimizeButton.hidden = true;
+            
+            // Hide the zoom button
+            var maximizeButton : NSButton = windowArray[currentWindow].standardWindowButton(NSWindowButton.ZoomButton)!;
+            maximizeButton.hidden = true;
+        }
+        
+        // If we said to floa tthe window...
+        if(floats) {
+            // Set its level to 200, it floats above EVERYTHING
+            windowArray[currentWindow].level = 200;
+        }
+        
+        // If we said for the window to move tp the active space...
+        if(joins) {
+            // Set the collection behaviour to can join all spaces
+            windowArray[currentWindow].collectionBehavior = NSWindowCollectionBehavior.CanJoinAllSpaces;
+        }
+        
+        // If we said not to have a shadow...
+        if(!shadow) {
+            // Disable the shadow
+            windowArray[currentWindow].hasShadow = false;
+        }
+        
+        // If we said to let us click through...
+        if(clickThrough) {
+            // Let us click ttough
+            windowArray[currentWindow].ignoresMouseEvents = true;
+        }
+        
+        // Set the window size to the image size in pixels
+        windowArray[currentWindow].setFrame(NSRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height), display: false);
+        
+        // Get the file extension
+        var imageNameAndExtension : String = url.lastPathComponent!;
+        var imageNameAndExtensionSplit : [String] = split(imageNameAndExtension){$0 == "."}
+        var imageExtension : String = imageNameAndExtensionSplit[imageNameAndExtensionSplit.count - 1];
+        
+        // If its not a GIF...
+        if(imageExtension != "gif") {
+            // Set the max size to 2x
+            windowArray[currentWindow].contentMaxSize = NSSize(width: newImage.size.width * 2, height: newImage.size.height * 2);
+        }
+        else {
+            // It is a gif, make the window unsizable
+            windowArray[currentWindow].contentMinSize = NSSize(width: newImage.size.width, height: newImage.size.height);
+            windowArray[currentWindow].contentMaxSize = NSSize(width: newImage.size.width, height: newImage.size.height);
+        }
+        
+        // Set the window aspect ratio to that of the image, so we can scale the window nicely
+        windowArray[currentWindow].contentAspectRatio = NSSize(width: newImage.size.width, height: newImage.size.height);
+        
+        // Add the image view to the window view
+        windowArray[currentWindow].contentView.addSubview(imageViewArray[currentWindow]);
+        
+        // Hide the titlebar
+        windowArray[currentWindow].titlebarAppearsTransparent = true;
+        
+        // Set the window to be transparent
+        windowArray[currentWindow].opaque = false;
+        
+        // Let the window be moved by its background (BROKEN)
+        windowArray[currentWindow].movableByWindowBackground = true;
+        
+        // Set the winddow background color
+        windowArray[currentWindow].backgroundColor = bgColor;
+        
+        // Center the window
+        windowArray[currentWindow].center();
+        
+        //Make the new window key
+        windowArray[currentWindow].makeKeyAndOrderFront(nil);
+        
+        // Add 1 to currentWindow
+        currentWindow += 1;
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # KISSViewer
-A KISS image viewer for OSX
+A KISS image viewer made for OSX
+
+### Features:
+- A borderless rounded image window
+- Lots of options
+- Supports every format NSImageView supports (Which is alot)
+
+### TODO:
+
+- Better GIF support (NSImageView is bad with GIFS)
+- Open online images
