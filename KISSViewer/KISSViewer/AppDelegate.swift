e0408e9901348328ca89c6635a1daa79ee2358b1
DrabWeb <sethunity@gmail.com>
2015-11-30 18:58:37 -0400

    Added support for multiple images (By request)

--- KISSViewer/KISSViewer/AppDelegate.swift
+++ KISSViewer/KISSViewer/AppDelegate.swift
@@ -35,6 +35,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // The modal that is shown when the user is opening an image
     var openPanel : NSOpenPanel = NSOpenPanel();
     
+    // Used for storing the opened images when the user does multiples of them
+    var openImages : [NSImage] = [NSImage()];
+    
+    // Used for scrolling through images while knowing where we are
+    var currentImageIndex = 0;
+    
     // Menu items
     
     // Float
@@ -105,6 +111,45 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             backgroundMenuItem.state = 1;
         }
     }
+    
+    // Next image
+    @IBOutlet weak var nextImageMenuItem: NSMenuItem!
+    @IBAction func nextImageMenuItemTriggered(sender: AnyObject) {
+        print("Next image");
+        
+        // If we add 1 from currentImageIndex and its less than the amount of images we opened...
+        if(currentImageIndex + 1 < openImages.count) {
+            // Add 1 to currentImageIndex
+            currentImageIndex++;
+        }
+        else {
+            // Set currentImageIndex
+            currentImageIndex = 0;
+        }
+        
+        // Open the image we just changed to
+        openImage(openImages[currentImageIndex], clearBackground: false, resize: false);
+    }
+    
+    // Previous image
+    @IBOutlet weak var previousImageMenuItem: NSMenuItem!
+    @IBAction func previousImageMenuItemTriggered(sender: AnyObject) {
+        print("Previous image");
+        
+        // If we remove 1 from currentImageIndex and its greater than or equal to 0...
+        if(currentImageIndex - 1 >= 0) {
+            // Subtract 1 from currentImageIndex
+            currentImageIndex--;
+        }
+        else {
+            // Set currentImageIndex to the length of the openImages array
+            currentImageIndex = openImages.count - 1;
+        }
+        
+        // Open the image we just changed to
+        openImage(openImages[currentImageIndex], clearBackground: false, resize: false);
+    }
+    
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -119,25 +164,57 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
             // It isnt, use the usual GUI
         else {
+            // Allow the open panel to allow multiple image selections
+            openPanel.allowsMultipleSelection = true;
+            
             // Set the open panels allowed file types to only images
             openPanel.allowedFileTypes = ["png", "tiff", "jpg", "gif", "jpeg"];
             
             // Show the open panel modal
             openPanel.runModal();
             
-            // Print the URl of the selected file, for debugging
-            print(openPanel.URL!);
+            // Print the URLs of the selected files, for debugging
+            print(openPanel.URLs);
             
-            // Create the NSImage that we are going to show in the image view
-            var image : NSImage = NSImage(contentsOfURL: openPanel.URL!)!;
+            print(openPanel.URLs.count);
             
-            // Open the image
-            openImage(image, clearBackground: false);
+            // If we selected multiple images...
+            if(openPanel.URLs.count > 1) {
+                // Create a variable to store the image array to load
+                var images : [NSImage]! = [NSImage()];
+                
+                // Go through the URL of every selected file from the open panel
+                for(var iterateUrls = 0; iterateUrls < openPanel.URLs.count; iterateUrls++) {
+                    // Create the NSImage that we are going to show in the image view
+                    var image : NSImage = NSImage(contentsOfURL: openPanel.URLs[iterateUrls])!;
+                    
+                    // Load the image at this URL and add it to the images array
+                    images.append(image);
+                }
+                
+                // For some reason the first image is null, lets remove it
+                images.removeAtIndex(0);
+                
+                // Open the images
+                openImages(images, clearBackground: false);
+            }
+            // If we only selected one image...
+            else {
+                // Disable the next and previous image menu items
+                nextImageMenuItem.hidden = true;
+                previousImageMenuItem.hidden = true;
+                
+                // Create the NSImage that we are going to show in the image view
+                var image : NSImage = NSImage(contentsOfURL: openPanel.URLs[0])!;
+                
+                // Open the image
+                openImage(image, clearBackground: false, resize: true);
+            }
         }
     }
     
-    // Opens the specified image, and it also makes the window background transparent if clearBackground is set to true
-    func openImage(image : NSImage, clearBackground : Bool) {
+    // Opens the specified image, and it also makes the window background transparent if clearBackground is set to true. Also, if you set resize to false, it wont resize it
+    func openImage(image : NSImage, clearBackground : Bool, resize : Bool) {
         // If we said to have a clear background...
         if(clearBackground == true) {
             // Set the window so it can be rendered transparent
@@ -153,17 +230,58 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Set the image view image
         imageView.image = image;
         
+        // Store the current window height and origin for when we are doing multiple images
+        var oldHeight = window.frame.height;
+        var oldOrigin = window.frame.origin;
+        
         // Set the window frame to match the image size
         window.setFrame(NSRect(x: 0, y: 0, width: image.size.width, height: image.size.height), display: false);
         
         // Set the window aspect ratio to the image size
         window.aspectRatio = NSSize(width: window.frame.width, height: window.frame.height);
         
-        // Center the window
-        window.center();
+        // If we dont want to resize...
+        if(!resize) {
+            // Get the aspect ratio of the image
+            var aspectRatio = image.size.width / image.size.height;
+            
+            // Figure out what the width would be if we kept the aspect ratio and set the height to the windows old height
+            var width = aspectRatio * oldHeight;
+            
+            // Set the windows frame tobe similar to the last one
+            window.setFrame(NSRect(x: oldOrigin.x, y: oldOrigin.y, width: width, height: oldHeight), display: false);
+            
+        }
+        
+        // If we want to resize...
+        if(resize) {
+            // Center the window
+            window.center();
+        }
         
         // Order the window to the front
         window.makeKeyAndOrderFront(self);
+    }
+    
+    // Opens the specified images, allows the user to scroll through them with the arrow keys, and it also makes the window background transparent if clearBackground is set to true
+    func openImages(images : [NSImage], clearBackground : Bool) {
+        print("Opening images...");
+        // If we said to have a clear background...
+        if(clearBackground == true) {
+            // Set the window so it can be rendered transparent
+            window.opaque = false;
+            
+            // Set the window background color to clear
+            window.backgroundColor = NSColor.clearColor();
+            
+            // Hide the effect view
+            visualEffectView.hidden = true;
+        }
+        
+        // Set the openImages to the images we are opening
+        openImages = images;
+        
+        openImage(openImages[0], clearBackground: clearBackground, resize: true);
     }
     
     // Use this when we know that /tmp/kissviewer exists, meaning it was launched from the command line
@@ -210,7 +328,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.hasShadow = imageShadow;
         
         // Load the image
-        openImage(NSImage(contentsOfURL: imageURL)!, clearBackground: imageTransparent);
+        openImage(NSImage(contentsOfURL: imageURL)!, clearBackground: imageTransparent, resize: true);
         
         // Set the scale
         window.setFrame(NSRect(x: 0, y: 0, width: window.frame.width * imageScale, height: window.frame.height * imageScale), display: false);
--- README.md
+++ README.md
@@ -5,19 +5,16 @@ A KISS image viewer for OSX.
 
 ### Features
 - A minimal, borderless rounded image window
+- Open multiple images and scroll through them (Great for Manga)
 - Lots of options 
 - Transparent window backgrounds
 
 ### Usage
 
 #### GUI
-Just run the app bundle, and you will be prompted to open a file
+Just run the app bundle, and you will be prompted to open a file, of which you can open 1 or more images.
 
 #### Command Line
 1. Copy Command Line / kissviewer to somewhere you can access it easily (Probably in your PATH)
 2. Edit it so that APPLICATIONPATH is the path to the KISSViewer.app bundle
 3. Run kissviewer from the command line (The variables are explained in the bash script)
-
-### TODO:
-
-- Better GIF support (NSImageView is bad with GIFS)
